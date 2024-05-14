1d1a57c495759d175a76f1b243eceb3fe126a176
swati6198 <146353034+swati6198@users.noreply.github.com>
2024-05-14 16:55:49 +0530

    comment added

--- config.inc.php
+++ config.inc.php
@@ -12,6 +12,13 @@ $username = "vacaysitters";
 $password = "vacaysitters";
 $dbname = "vacaysitters";
 
+
+/*
+comment section
+*/
+
+
+
 try {
     	$connection = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
